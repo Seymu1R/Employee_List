8216d568b82b1b0a0577a2ed06f33d93b86300cc
Seymu1R <102611811+Seymu1R@users.noreply.github.com>
2022-08-24 13:08:27 +0400

    adding delete and edit for adding content

--- assets/main.js
+++ assets/main.js
@@ -8,6 +8,8 @@ tableAdded.innerHTML = `
            <th>Name</th>
            <th>Surname</th>
            <th>Salary</th>
+           <th><button class="sort-btn" >Sort</button></th>
+           <th><button class="filter-btn" >Filter</button></th>
            </tr>
            
 
@@ -19,11 +21,28 @@ addBtn.addEventListener("submit", () => {
     tableAdded.innerHTML += `
     <tr  >
     <td> <input id="content-name" value="${inputName}" disabled type="text"> </td>
-    <td><input id="content-surname"value="${inputSurname}"type="text" disabled ></td>
+    <td><input id="content-surname" value="${inputSurname}" type="text" disabled ></td>
     <td> <input  id="content-salary" type="number" value="${inputSalary}" disabled ></td>
-    <td><button class="delete-btn" >Delete</button> </td>
-    <td><button class="edit-btn" >Edit</button> </td>
+    <td><button class="delete-btn" onClick="employeeDelete(event)" >Delete</button> </td>
+    <td><button class="edit-btn" onClick="employeeEdit(event)" >Edit</button> </td>
     </tr> 
    `
 
 })
+
+let employeeDelete=(event)=>{
+  event.target.parentElement.parentElement.remove();
+}
+let employeeEdit=(event)=>{
+   let parent=event.target.parentElement.parentElement;
+   inputList=parent.querySelectorAll("input");
+   inputList.forEach(input => {
+    let disabledInp=input.getAttribute("disabled"); 
+    if (disabledInp != "" && disabledInp == null) {
+        input.setAttribute("disabled", "disabled");
+      } else {
+        input.removeAttribute("disabled");
+      }   
+   });
+   
+}
